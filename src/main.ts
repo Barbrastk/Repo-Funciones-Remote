6fdfec6898c60694e739d0f4d485865f875afffa
Bárbara Starck <147934490+Barbrastk@users.noreply.github.com>
2025-11-03 12:17:08 +0100

    changes on main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ const numeroTurno = document.getElementById("numero-turno") as HTMLElement;
 const btnAnterior = document.getElementById("anterior") as HTMLButtonElement;
 const btnReset = document.getElementById("reset") as HTMLButtonElement;
 const btnSiguiente = document.getElementById("siguiente") as HTMLButtonElement;
+const inputTurno = document.getElementById("nuevo-turno") as HTMLInputElement;
+const btnCambiar = document.getElementById("cambiar") as HTMLButtonElement;
 
+//Creo las funciones
 function actualizarDisplay(): void {
     numeroTurno.textContent = turnoActual.toString().padStart(2, "0");
 }
@@ -36,5 +39,19 @@ function siguienteTurno (): void {
 }
 
 btnSiguiente.addEventListener("click", siguienteTurno);
+
+function cambiarTurno (): void {
+    const nuevoTurno = parseInt(inputTurno.value);
+
+    if (!isNaN(nuevoTurno) && nuevoTurno >=0) {
+        turnoActual = nuevoTurno;
+        actualizarDisplay();
+    } else{
+        alert("Por favor introduce un nuevo número válido mayor o igual a 0.");
+    }
+    inputTurno.value = ""; //Limpia el campo de texto
+}
+
+btnCambiar.addEventListener("click", cambiarTurno);
 
 actualizarDisplay();
